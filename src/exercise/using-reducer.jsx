b5719331747292710b6920003d7e4cf387874ed5
Victor Olateju <victor.olateju@matblas.com>
2025-01-31 14:59:11 +0000

    added workflow

--- src/exercise/using-reducer.jsx
+++ src/exercise/using-reducer.jsx
@@ -3,7 +3,7 @@ import { useReducer } from "react"
 const reducerFn = (state, action) => {
     if(action.type === "start") return {money: state.money - 10}
     if(action.type === "end") return {money: state.money + 10}
-
+    if(action.type === "unknown") return {money: state.money * 2}
     return state
 }
 
@@ -16,5 +16,6 @@ export function ReducerTask () {
         <h1>Wallet: {state.money}</h1>
         <button onClick={()=> dispatch({type: "start"})}>Subtract</button>
         <button onClick={()=> dispatch({type: "end"})}>Add</button>
+        <button onClick={()=> dispatch({type: "unknown"})}>Add</button>
     </div>
 }
