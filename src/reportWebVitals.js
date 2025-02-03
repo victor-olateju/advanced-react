0c4ab0be7c0fb205803eb63548f001caa782f339
Victor Olateju <victor.olateju@matblas.com>
2025-02-03 11:00:05 +0000

    removed vitals and setup

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+export const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -10,6 +10,4 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
-export default reportWebVitals;
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="e2272d40-e23a-51d5-86a9-12e1af1cf6c7")}catch(e){}}();
-//# debugId=e2272d40-e23a-51d5-86a9-12e1af1cf6c7
+
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="c094c2f8-aa68-5d46-890f-53284610e6b9")}catch(e){}}();
-//# debugId=c094c2f8-aa68-5d46-890f-53284610e6b9
+// import '@testing-library/jest-dom';
+// !function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="c094c2f8-aa68-5d46-890f-53284610e6b9")}catch(e){}}();
+// //# debugId=c094c2f8-aa68-5d46-890f-53284610e6b9
